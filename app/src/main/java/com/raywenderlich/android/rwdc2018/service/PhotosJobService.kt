404405c36f66d13596d5043ff0c10ea39ef6de05
Henry AT <yiperu@gmail.com>
2019-09-28 11:50:03 -0300

    Delete body and change pather class to JobService

--- app/src/main/java/com/raywenderlich/android/rwdc2018/service/PhotosJobService.kt
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/service/PhotosJobService.kt
@@ -1,12 +1,11 @@
 package com.raywenderlich.android.rwdc2018.service
 
 import android.app.Service
+import android.app.job.JobService
 import android.content.Intent
 import android.os.IBinder
 
-class PhotosJobService : Service() {
+class PhotosJobService : JobService() {
 
-  override fun onBind(intent: Intent): IBinder {
-    TODO("Return the communication channel to the service.")
-  }
+
 }

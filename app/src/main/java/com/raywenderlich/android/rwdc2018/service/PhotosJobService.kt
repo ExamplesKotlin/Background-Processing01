e13931e6393aaaf559d6070d6291b3de594c92c6
Henry AT <yiperu@gmail.com>
2019-09-28 12:56:47 -0300

    Implement onStartJob and onStopJob function in PhotosJobService

--- app/src/main/java/com/raywenderlich/android/rwdc2018/service/PhotosJobService.kt
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/service/PhotosJobService.kt
@@ -1,13 +1,26 @@
 package com.raywenderlich.android.rwdc2018.service
 
 import android.app.Service
+import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class PhotosJobService : JobService() {
 
   companion object {
     private const val TAG = "PhotosJobService"
   }
+
+  override fun onStartJob(params: JobParameters?): Boolean {
+    return true
+  }
+
+  override fun onStopJob(params: JobParameters?): Boolean {
+    Log.i(TAG, "Job stopped: " + params?.jobId)
+    return false
+  }
+
+
 }

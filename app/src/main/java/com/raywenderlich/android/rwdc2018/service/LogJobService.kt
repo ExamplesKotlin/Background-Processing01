de76ba8dd62780c37759da226af852c5009d393a
Henry AT <yiperu@gmail.com>
2019-10-01 22:28:08 -0300

    🏗  Implement onStartJob and onStopJob

--- app/src/main/java/com/raywenderlich/android/rwdc2018/service/LogJobService.kt
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/service/LogJobService.kt
@@ -5,6 +5,7 @@ import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class LogJobService : JobService() {
 
@@ -13,10 +14,20 @@ class LogJobService : JobService() {
   }
 
   override fun onStartJob(params: JobParameters?): Boolean {
+
+    val runnable = Runnable {
+      Thread.sleep(10000)
+      jobFinished(params, false)
+      Log.i(TAG, "Job finished: " + params?.jobId)
+    }
+
+    Log.i(TAG, "Starting Job: " + params?.jobId)
+    Thread(runnable).start()
     return true
   }
 
   override fun onStopJob(params: JobParameters?): Boolean {
+    Log.i(TAG, "Stopping Job: " + params?.jobId)
     return false
   }
 

97af1e37da4726d4a73020f0632e8921d8a05263
Henry AT <yiperu@gmail.com>
2019-10-01 22:22:39 -0300

    👨🏻‍💻  Add companion object with TAG to LogJobService class and implement abstract function onStartJob, onStopJob

--- app/src/main/java/com/raywenderlich/android/rwdc2018/service/LogJobService.kt
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/service/LogJobService.kt
@@ -1,13 +1,23 @@
 package com.raywenderlich.android.rwdc2018.service
 
 import android.app.Service
+import android.app.job.JobParameters
 import android.app.job.JobService
 import android.content.Intent
 import android.os.IBinder
 
 class LogJobService : JobService() {
 
-  override fun onBind(intent: Intent): IBinder {
-    TODO("Return the communication channel to the service.")
+  companion object {
+    private const val TAG = "LogJobService"
   }
+
+  override fun onStartJob(params: JobParameters?): Boolean {
+    return true
+  }
+
+  override fun onStopJob(params: JobParameters?): Boolean {
+    return false
+  }
+
 }

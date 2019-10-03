93ddc06feee2a243f39c033b24f80b69fbbf3969
Henry AT <yiperu@gmail.com>
2019-10-02 23:24:43 -0300

    Make some function for DownloadIntentService class and implement its methods

--- app/src/main/java/com/raywenderlich/android/rwdc2018/service/DownloadIntentService.kt
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/service/DownloadIntentService.kt
@@ -1,9 +1,31 @@
 package com.raywenderlich.android.rwdc2018.service
 
 import android.app.IntentService
+import android.content.Intent
+import android.util.Log
+import com.raywenderlich.android.rwdc2018.app.SongUtils
 
 
 class DownloadIntentService : IntentService("DownloadIntentService") {
 
+  companion object {
+    private const val TAG = "DownloadIntentService"
+    private const val ACTION_DOWNLOAD = "ACTION_DOWNLOAD"
+    private const val EXTRA_URL = "EXTRA_URL"
+  }
+
+  override fun onHandleIntent(intent: Intent?) {
+    when (intent?.action) {
+      ACTION_DOWNLOAD -> {
+        // TODO
+      }
+    }
+  }
+
+  private fun handleActionDownload(param: String) {
+    Log.i(TAG, "Starting download for $param")
+    SongUtils.download(param)
+    Log.i(TAG ,"Ending download for $param")
+  }
 
 }

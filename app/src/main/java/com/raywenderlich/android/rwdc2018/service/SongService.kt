df34c9f23ec76dab0a55b471119c0339e80f0a00
Henry AT <yiperu@gmail.com>
2019-10-04 00:21:01 -0300

    🆕  Create a new SongService service and collapse in Manifest

---
+++ app/src/main/java/com/raywenderlich/android/rwdc2018/service/SongService.kt
@@ -0,0 +1,12 @@
+package com.raywenderlich.android.rwdc2018.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SongService : Service() {
+
+  override fun onBind(intent: Intent): IBinder {
+    TODO("Return the communication channel to the service.")
+  }
+}
